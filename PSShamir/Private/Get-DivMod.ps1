924e947efa053746932fcf83791ebf32f1bfd7c1
Constantin Heinzler <me@cheinzler.de>
2024-11-17 11:14:03 +0100

    Remove test line

--- PSShamir/Private/Get-DivMod.ps1
+++ PSShamir/Private/Get-DivMod.ps1
@@ -6,7 +6,6 @@ Function Get-DivMod {
     )
 
     $Inverse = (Get-ExtGreatestCommonDivisor -a $Denominator -b $Prime)[0]
-    $InverseNew = [bigint]::GreatestCommonDivisor($Denominator, $Prime)
 
     $Output = $Numerator * $Inverse
 
