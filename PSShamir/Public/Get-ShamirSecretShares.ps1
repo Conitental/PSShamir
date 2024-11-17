55f9ca94624d815af6d23b987731baa8624b47d2
Constantin Heinzler <me@cheinzler.de>
2024-11-17 12:03:40 +0100

    Add lower bound to share numbers

--- PSShamir/Public/Get-ShamirSecretShares.ps1
+++ PSShamir/Public/Get-ShamirSecretShares.ps1
@@ -26,7 +26,9 @@ Function Get-ShamirSecretShares {
     Param(
         [Parameter(Mandatory, ValueFromPipeline)]
         [String]$Secret,
+        [ValidateRange(2, [Int]::MaxValue)]
         [Int]$Shares = 7,
+        [ValidateRange(2, [Int]::MaxValue)]
         [Int]$MinimumShares = 3
     )
 
