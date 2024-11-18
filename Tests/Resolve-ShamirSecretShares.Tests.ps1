f878539f34850b8988a5c745cf50934948571bec
Conitental <34301746+Conitental@users.noreply.github.com>
2024-11-18 10:34:31 +0100

    Add tests for property validation

--- Tests/Resolve-ShamirSecretShares.Tests.ps1
+++ Tests/Resolve-ShamirSecretShares.Tests.ps1
@@ -11,6 +11,12 @@ Describe "Resolve-ShamirSecretShares" {
         Resolve-ShamirSecretShares $Shares | Should -Be $Secret
 	}
 
+	It "should return the original secret from only the strings" {
+        $Secret = "🦆"
+		$Shares = Get-ShamirSecretShares -Secret $Secret
+        Resolve-ShamirSecretShares $Shares.Share | Should -Be $Secret
+	}
+
 	It "should return a serialization error" {
 		{ Resolve-ShamirSecretShares -Shares "I will peng 🐧"} | Should -Throw -ExpectedMessage "Could not unserialize share data"
 	}
@@ -18,5 +24,17 @@ Describe "Resolve-ShamirSecretShares" {
 	It "should return that the secret could not be recovered" {
         $Shares = Get-ShamirSecretShares -Secret "Yo soy una tortuga 🐢" -MinimumShares 4
 		{ Resolve-ShamirSecretShares -Shares $Shares[0..2]} | Should -Throw -ExpectedMessage "Could not resolve secret*"
+	}
+
+	It "should throw a missing property error" {
+        {
+            Resolve-ShamirSecretShares (@{Id=1; NotData=''} | ConvertTo-SerializedObject)
+        } | Should -Throw -ExpectedMessage "Passed object does not contain required property Data"
+	}
+
+	It "should throw a missing property error" {
+        {
+            Resolve-ShamirSecretShares (@{Id=11; Data='ImNotTheDataYouExpected'} | ConvertTo-SerializedObject)
+        } | Should -Throw -ExpectedMessage "Share 11 does not contain required property chunkindex*"
 	}
 }
